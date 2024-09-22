db8a13d058c3930070a9437f4aa2e3481b897b14
lyc2028 <155152035+lyc2028@users.noreply.github.com>
2024-09-22 13:35:10 +0800

    Update problemset.js

--- csp-r1/problemset.js
+++ csp-r1/problemset.js
@@ -1,3 +1,26 @@
+/*
+MIT License
+
+Copyright (c) 2024 lyc2028
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 //试卷标题、难度、年份、类型、是否隐藏
 var strlist = ["CSP-J1 2021", "CSP-S1 2021", "CSP-J1 2022", "CSP-S1 2022", "洛谷 SCP 2023 第一轮（初赛 J 组）模拟", "洛谷 SCP 2023 第一轮（初赛 S 组）模拟", "20240802S组初赛模拟", "20240808S组初赛模拟", "洛谷 SCP 2024 第一轮（初赛 J 组）模拟", "洛谷 SCP 2024 第一轮（初赛 S 组）模拟"];
 var diff = [0, 1, 0, 1, 0, 1, 1, 1, 0, 1];
