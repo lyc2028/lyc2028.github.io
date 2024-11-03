2009944449a34f3122c4c7a7d5032ebaf286bc1e
lyc2028 <155152035+lyc2028@users.noreply.github.com>
2024-11-03 14:42:40 +0800

    Update version.js

--- app/slide-player/version.js
+++ app/slide-player/version.js
@@ -1,3 +1,5 @@
 //MIT License: https://github.com/lyc2028/lyc2028.github.io/blob/main/LICENSE
 var latestVersion = "1.0.0";
 var latestPreviewVersion = "1.0.0";
+var latestVersionDescription = "初始版本发布";
+var latestPreviewVersionDescription = "初始版本发布";
